067084f0d863250102f07a123e9c3d46e7ac3bc5
Felipe Medeiros Malentaqui <102374645+FelipeMalentaqui@users.noreply.github.com>
2022-11-03 22:23:04 -0300

    Add files via upload

---
+++ exer03.py
@@ -0,0 +1,17 @@
+#criando uma variavel
+#trabalhando com variaveis 
+
+#num1 = 10 
+#num2 = 5
+
+num1 = input('informe o primeiro valor:')
+num2 = input('informe o segundo valor:')
+soma = num1 + num2
+
+print(type(num1))
+
+#print('A soma é: ', soma)
+
+soma1 = int(num1) + int(num2)
+
+print('A soma é: {}' . format(soma1))
