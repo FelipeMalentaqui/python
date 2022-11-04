067637ecfa60e9bcdb745f9da9c60c49f7c5938d
Felipe Medeiros Malentaqui <102374645+FelipeMalentaqui@users.noreply.github.com>
2022-11-03 22:03:24 -0300

    Add files via upload

---
+++ exer01.py
@@ -0,0 +1,6 @@
+# caracter de comentario
+#print para inprimir informações na tela
+
+print('Olá mundo python')
+
+print('Meu nome é: ', 'Felipe','e tenho', 26 , 'anos')
---
+++ exer02.py
@@ -0,0 +1,12 @@
+# usando input para entrada de dados
+# o input é pra receber um dado
+
+print('seja bem-vindo a aula de ' + 'python')
+
+# as chaves ira nos indicar uma posição de susbstituição 
+
+print('Ola {}' . format(input('Qual o seu nome?')))
+
+# o que significa format e input ?
+
+
